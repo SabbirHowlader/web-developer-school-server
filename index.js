72b6385785d15cb53ab1195eaaa92662149fbf0b
SabbirHowlader <sabbirahmedreza56@gmail.com>
2022-10-27 00:12:34 +0600

    added id

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use (cors());
 
 const categories = require('./data/categories.json');
+const card = require('./data/card.json');
 
 app.get('/', (req, res) => {
     res.send('Developer Api Running');
@@ -11,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/card-categories', (req, res) =>{
     res.send(categories)
+});
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    const allCard = card.filter(n=> n.category_id === id);
+    res.send(allCard);
 });
 
 app.listen (port, () => {
