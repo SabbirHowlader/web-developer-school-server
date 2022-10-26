4476936e55e8c48e58b68c2dda9cbdabd9a95b7d
SabbirHowlader <sabbirahmedreza56@gmail.com>
2022-10-26 20:28:21 +0600

    added Categories

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const categories = require('./data/categories.json');
+
 app.get('/', (req, res) => {
     res.send('Developer Api Running');
+});
+
+app.get('/card-categories', (req, res) =>{
+    res.send(categories)
 });
 
 app.listen (port, () => {
